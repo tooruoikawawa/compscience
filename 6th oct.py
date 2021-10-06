028de8753042ddd693b19667e1072791c61c3a97
tooruoikawawa <79664905+tooruoikawawa@users.noreply.github.com>
2021-10-06 19:00:35 +0100

    Add files via upload

---
+++ 6th oct.py
@@ -0,0 +1,32 @@
+import random
+
+n1 = random.randint(0, 12) 
+n2 = random.randint(0, 12) 
+ans = n1*n2
+print("%d * %d" %(n1,n2)) # find out the question to make life easier
+
+keepGoing = True
+
+while  keepGoing:  
+    guess = int(input("Guess the product of two numbers from 0 to 12 "))
+    
+    if guess == ans:
+        print("You are correct!")
+        playAgain = input("Would you like to play again? Y or N")
+        if playAgain.upper() == "N":
+            keepGoing = False
+        elif playAgain.upper() == "Y":
+            n1 = random.randint(0, 12)
+            n2 = random.randint(0, 12)
+            ans = n1*n2
+            print("%d * %d" %(n1,n2))
+            
+    elif guess < ans:
+        print("That's too low")
+        
+    else:
+        print("That's too high")
+    
+    
+print("The correct answer was %d" %(n1*n2))
+print("Goodbye :)")
