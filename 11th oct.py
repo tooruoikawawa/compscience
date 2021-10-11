467ae7d80912a9b4fdcf5c978db18b7b6acf5389
tooruoikawawa <79664905+tooruoikawawa@users.noreply.github.com>
2021-10-11 12:10:55 +0100

    Add files via upload

---
+++ 11th oct.py
@@ -0,0 +1,31 @@
+# page 30 questions
+'''
+1. George Boole was an English mathematician and worked at the Queen's College in Cork
+2. Boolean Logic.
+3. They are used for comparing one item to another and recording the result.
+4. == - is equal to
+   != - is not equal to
+   < - less than
+5. True or False
+6. False
+7. Comparisons that give a True or False aswer ar Boolena expressions.
+8. An expression consists of values and operators and always evaluates to a single value.
+9. and, or, not
+10. To show every result that can occur when a Boolean operator is used.
+11.
+12. - True
+    - True
+    - False
+    - False
+13. A conditional is used for for testing if a Boolean expression is True or False.
+14. Branches in code are executed when the variable is True
+15. if
+16.
+17.
+18. When the conditional is false.
+19.
+20. after if and before else
+
+'''
+# tasks page 30-31
+
