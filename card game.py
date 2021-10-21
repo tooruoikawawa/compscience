2f6983ee3a41bddd8c6eba38f73c389150234b0a
tooruoikawawa <79664905+tooruoikawawa@users.noreply.github.com>
2021-10-21 14:12:54 +0100

    Add files via upload

---
+++ card game.py
@@ -0,0 +1,12 @@
+import random
+print("Get a random card out of a deck of cards!!")
+
+suits = [" hearts", " clubs", " spades", " diamonds"]
+a = random.randint(0, 3)
+faces = [" A", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9", " 10", " King", " Queen", " Jack"]
+b = random.randint(0, 12)
+
+for i in range(5):
+    print("You got: ", faces[b] + suits[a])
+      
+      
