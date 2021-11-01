b401e1bbcd52b218e249534169f304a8ad4de4c5
tooruoikawawa <79664905+tooruoikawawa@users.noreply.github.com>
2021-11-01 12:15:23 +0000

    Add files via upload

---
+++ page 46.py
@@ -0,0 +1,41 @@
+# page 46 questions
+
+'''
+1. A test case uses a set of conditions or inputs to test a piece of code.
+2. The purpose of running code with test cases is to test a range of values or inputs to cover all the possible values that
+    could be entered.
+3. ID, description, test data, expected result, actual result, pass y/n
+4. The difference is that the expected value is the correct value and the actual value is the output of the code.
+5. The golden rule is that the expected result mujst never be taken from the output of the code
+6. Code coverage is covering every case of the code
+7. Equivalence partitions are a techique that partitions the range of conditions/inputs in areas that should have the
+    same behaviour
+8. Extreme values are really high numbers
+9. Testing boundary values involves testing values in the region where partitions meet
+10. Error anticipation is knowing that the code will have a bug or wil go wrong
+11. There may be a higher number than there should be inputted
+12. To prevent bugs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
