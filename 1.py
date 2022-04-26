60fabec7834a9a631cd230adc57bdae4df457ec5
tooruoikawawa <79664905+tooruoikawawa@users.noreply.github.com>
2022-04-26 10:17:49 +0100

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,56 @@
+#page 202 + 203
+'''
+1. using a return value in a function may be more suitable than just displaying the result because you might
+   want to use it for some other purpose in the program
+2. return
+3. it is useful to return boolean values from a function because it allows us to control the flow of
+   a program
+4. - the function name
+   - the argument
+   - what does the function test
+   - the boolean value
+'''
+#task 1
+'''
+def valueOfPi():
+    return 22/7
+print(valueOfPi())
+'''
+#task 2
+
+def numbers():
+    a = int(input("Input a number: "))
+    b = int(input("Input a number: "))
+    num = a/b
+    return num
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
